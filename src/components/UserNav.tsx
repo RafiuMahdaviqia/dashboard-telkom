fd7f4689c9849be7ba3bd5862ddea62297023ed7
Rafi'u Mahdaviqia <rafi'u@polinema.ac.id>
2025-07-10 01:57:36 +0700

    Penambahan Fitur Login & Logout

---
+++ src/components/UserNav.tsx
@@ -0,0 +1,38 @@
+import { createClient } from "@/utils/supabase/server";
+import { redirect } from "next/navigation";
+import Link from "next/link";
+
+export default async function UserNav() {
+  const supabase = createClient();
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  const signOut = async () => {
+    "use server";
+
+    const supabase = createClient();
+    await supabase.auth.signOut();
+    return redirect("/login");
+  };
+
+  return user ? (
+    <div className="flex items-center gap-4">
+      {/* --- PERUBAHAN DI SINI --- */}
+      <span className="hidden text-sm sm:block text-gray-300">{user.email}</span>
+      <form action={signOut}>
+        <button className="rounded-md bg-gray-800 px-3 py-2 text-sm text-white hover:bg-gray-700">
+          Logout
+        </button>
+      </form>
+    </div>
+  ) : (
+    <Link
+      href="/login"
+      className="rounded-md bg-blue-600 px-3 py-2 text-sm text-white hover:bg-blue-700"
+    >
+      Login
+    </Link>
+  );
+}
